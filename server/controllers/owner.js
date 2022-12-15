47aa6c6c88a85885395cb28ee64f63c347be69b2
Lim Zheng Kai <zhengkai@Lims-MacBook-Air.local>
2022-12-15 16:19:34 +0800

    added login and sessions

--- server/controllers/owner.js
+++ server/controllers/owner.js
@@ -5,7 +5,9 @@ const Owner = require("../models/owner.js");
 
 owner.post("/signup", async (req, res) => {
   try {
-    const newHost = await Owner.create(req.body);
+    const data = req.body;
+    data.password = bcrypt.hashSync(data.password, 10);
+    const newHost = await Owner.create(data);
     res.status(200).send(newHost);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -14,13 +16,13 @@ owner.post("/signup", async (req, res) => {
 
 owner.get("/seed", async (req, res) => {
   const owner = {
-    username: "jayalalala",
-    password: "12345678",
+    username: "admin",
+    password: bcrypt.hashSync("12345678", 10),
     name: "Jaya Lala",
     email: "jajaja@hotmail.com",
   };
   try {
-    //await Owner.deleteMany({});
+    await Owner.deleteMany({});
     const newOwner = await Owner.create(owner);
     res.json(newOwner);
   } catch (error) {
@@ -32,6 +34,16 @@ owner.get("/accounts", async (req, res) => {
   try {
     const foundOwners = await Owner.find().exec();
     res.status(200).json(foundOwners);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+owner.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Owner.findById(id);
+    res.json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- server/controllers/renter.js
+++ server/controllers/renter.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const renter = express.Router();
 const Renter = require("../models/renter.js");
 
 renter.post("/signup", async (req, res) => {
   try {
-    const newRenter = await Renter.create(req.body);
+    const data = req.body;
+    data.password = bcrypt.hashSync(data.password, 10);
+    const newRenter = await Renter.create(data);
     res.status(200).send(newRenter);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -14,14 +17,14 @@ renter.post("/signup", async (req, res) => {
 renter.get("/seed", async (req, res) => {
   const renter = {
     username: "admin",
-    password: "12345678",
+    password: bcrypt.hashSync("12345678", 10),
     name: "Jun Jie",
     email: "jj@hotmail.com",
     creditcard: 4256293384759102,
     cvc: 321,
   };
   try {
-    //await Renter.deleteMany({});
+    await Renter.deleteMany({});
     const newRenter = await Renter.create(renter);
     res.json(newRenter);
   } catch (error) {
@@ -33,6 +36,16 @@ renter.get("/accounts", async (req, res) => {
   try {
     const foundRenters = await Renter.find().exec();
     res.status(200).json(foundRenters);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+renter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Renter.findById(id);
+    res.json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
